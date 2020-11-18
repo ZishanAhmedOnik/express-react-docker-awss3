1950d1de6ac21fdb065bb0e5521efefb08f4bcfc
Zishan Onik <zisanik@gmail.com>
2020-11-18 13:40:07 -0600

    mongodb added

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('dotenv').config()
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const index = require('./routes/index')
 const aws = require('./routes/aws');
@@ -9,6 +10,24 @@ const app = express()
 const port = process.env.PORT
 
 app.use(cors());
+
+let mongoURI = `mongodb://mongodb/${process.env.MONGODB_DB_NAME}`;
+mongoose.connect(mongoURI, {
+  auth: {
+    user: process.env.MONGODB_USERNAME,
+    password: process.env.MONGODB_PASSWORD
+  },
+  authSource: "admin",
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+}).then(
+  () => {
+    console.log('Connected to mongodb')
+  },
+  (err) => {
+    console.log(err);
+  }
+)
 
 app.use('/', index);
 app.use('/aws', aws);
--- backend/routes/aws.js
+++ backend/routes/aws.js
@@ -1,5 +1,6 @@
 let router = require('express').Router();
 let multer = require('multer');
+let multerS3 = require('multer-s3');
 let AWS = require('aws-sdk');
 let uuid = require('uuid/v4');
 
@@ -8,40 +9,43 @@ const s3 = new AWS.S3({
     secretAccessKey: process.env.AWS_SECRET
 })
 
-const storage = multer.memoryStorage({
-    destination: function(req, file, callback) {
-        callback(null, '')
-    }
-})
+// const storage = multer.memoryStorage({
+//     destination: function(req, file, callback) {
+//         callback(null, '')
+//     }
+// })
 
-const upload = multer({ storage: storage }).single('file')
+// const upload = multer({ storage: storage }).single('file')
+
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.AWS_BUCKET_NAME,
+        metadata: function (req, file, cb) {
+            cb(null, { fieldName: file.fieldname });
+        },
+        key: function (req, file, cb) {
+            let fileNameParts = file.originalname.split('.');
+            let fileType = fileNameParts[fileNameParts.length - 1];
+
+            cb(null, `${uuid()}.${fileType}`)
+        }
+    })
+}).single('file')
 
 router.post('/upload', upload, (req, res) => {
-    let fileNameParts = req.file.originalname.split('.');
-    let fileType = fileNameParts[fileNameParts.length - 1];
+    console.log(req.file);
 
-    let params = { 
-        Bucket: process.env.AWS_BUCKET_NAME, 
-        Key: `${uuid()}.${fileType}`, 
-        Body: req.file.buffer 
-    }
-
-    s3.upload(params, (err, data) => {
-        if(err) {
-            res.status(500).send(err);
-        }
-
-        res.send(data);
-    });
+    res.json({ 'status': 'OK' });
 });
 
 router.get('/list', (req, res) => {
     let params = {
         Bucket: process.env.AWS_BUCKET_NAME
     }
-    
+
     s3.listObjectsV2(params, (err, data) => {
-        if(err) {
+        if (err) {
             res.status(500).send(err);
         }
         else {

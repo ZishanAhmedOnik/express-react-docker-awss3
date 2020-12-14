74b838669de9b834ee6d6332b7b97c65f687ce74
Zishan Onik <zisanik@gmail.com>
2020-12-14 16:31:06 -0600

    added confirm password in validation

--- backend/helpers/validation_schema.js
+++ backend/helpers/validation_schema.js
@@ -5,7 +5,8 @@ const authSchema = Joi.object({
     firstName: Joi.string().required(),
     lastName: Joi.string().required(),
     email: Joi.string().email().lowercase().required(),
-    password: Joi.string().min(2).required() //min len for dev purpose, will be updated in production
+    password: Joi.string().min(2).required(), //min len for dev purpose, will be updated in production
+    confirmPassword: Joi.ref('password')
 })
 
 module.exports = {
